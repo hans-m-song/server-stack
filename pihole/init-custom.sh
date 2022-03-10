c92ecc119109b0b58c878d474a34daf685c9c6c5
Hans Song <hans.m.song@gmail.com>
2022-03-10 14:09:45 +0000

    cleanup, restore dynamic dns, public facing hello world

--- pihole/init-custom.sh
+++ pihole/init-custom.sh
@@ -3,6 +3,6 @@
 set -e
 set -o pipefail
 
-for alias in ${ALIASES}; do
+for alias in ${PIHOLE_ALIASES}; do
     grep -q ${alias} /etc/pihole/custom.list || echo "${HOST_IP} ${alias}.${HOST_NAME}" >>/etc/pihole/custom.list
 done
